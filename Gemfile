30016ddde7b063e35b2d6e55905d7ffc62398b3b
Simon Lacroix <simon@miaou.be>
2016-12-22 17:44:00 +0100

    Implement game endpoint

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.1'
 
 gem 'grape'
+gem 'grape-entity'
 gem 'puma', '~> 3.0'
 gem 'sqlite3'
 
--- app/apis/hexago_api.rb
+++ app/apis/hexago_api.rb
@@ -1,2 +1,18 @@
 class HexagoAPI < Grape::API
+  version 'v1', using: :header, vendor: 'HexaGo'
+  format :json
+
+  resource :games do
+    route_param :game_id do
+      before do
+        @game = Game.find params[:game_id]
+      end
+
+      desc 'Fetch a game'
+      get do
+        { id: @game.id }
+        present @game, with: Entities::GameEntity
+      end
+    end
+  end
 end
