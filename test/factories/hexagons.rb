2a3395bd90061019059ffc598250a13f26f9bc7b
Simon Lacroix <simon@miaou.be>
2016-12-26 20:13:05 +0100

    Add validations on Hexagon (#5)

--- test/factories/hexagons.rb
+++ test/factories/hexagons.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :hexagon do
     x { rand(10) }
     y { rand(10) }
-    z { rand(10) }
 
     association :game
   end
--- test/models/hexagon_test.rb
+++ test/models/hexagon_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class HexagonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validate coordinates' do
+    assert build(:hexagon, x: 0, y: 0, z: 0).valid?
+    refute build(:hexagon, x: 0, y: 1, z: 0).valid?
+  end
+
+  test 'validate uniqueness of a hexagon in a game' do
+    game = create(:game)
+    game.hexagons.destroy_all
+
+    assert build(:hexagon, game: game, x: 0, y: 0).save
+    refute build(:hexagon, game: game, x: 0, y: 0).save
+
+    assert build(:hexagon, x: 0, y: 0).save
+
+    assert build(:hexagon, game: game, x: 0, y: 1).save
+    assert build(:hexagon, game: game, x: 1, y: 0).save
+  end
 end
