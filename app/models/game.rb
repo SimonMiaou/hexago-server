be5862b8c1868cecfc073455867e55090be6f99a
Simon Lacroix <simon@miaou.be>
2016-12-27 00:07:26 +0100

    Validate number of players

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,5 +5,7 @@ class Game < ApplicationRecord
 
   validates :board_size, numericality: { greater_than_or_equal_to: 3 },
                          presence: true
-  validates :number_of_players, presence: true
+  validates :number_of_players, numericality: { greater_than_or_equal_to: 2,
+                                                less_than_or_equal_to: 6 },
+                                presence: true
 end
