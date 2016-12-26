b3260c0c4a0644538f7c4f62573089578f6b13e6
Simon Lacroix <simon@miaou.be>
2016-12-26 17:46:55 +0100

    Validate hexagon coordinates

--- app/models/hexagon.rb
+++ app/models/hexagon.rb
@@ -1,4 +1,22 @@
 class Hexagon < ApplicationRecord
   belongs_to :game
   belongs_to :user, optional: true
+
+  validates :x, :y, :z, presence: true
+  validate :validate_coords
+
+  before_validation :init_z_if_empty
+
+  private
+
+  def init_z_if_empty
+    return if z.present?
+    return if x.blank? || y.blank?
+    self.z = 0 - x - y
+  end
+
+  def validate_coords
+    return if x.blank? || y.blank? || z.blank?
+    errors.add(:base, 'x + y + z must equal 0') if (x + y + z).nonzero?
+  end
 end
--- test/models/hexagon_test.rb
+++ test/models/hexagon_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class HexagonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'validate coordinates' do
+    assert build(:hexagon, x: 0, y: 0, z: 0).valid?
+    refute build(:hexagon, x: 0, y: 1, z: 0).valid?
+  end
 end
