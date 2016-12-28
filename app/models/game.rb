a8aa43bd4a55e750350996148bd947a9071e8d70
Simon Lacroix <simon@miaou.be>
2016-12-28 03:08:02 +0100

    Automatically create hexagons

--- app/models/game.rb
+++ app/models/game.rb
@@ -8,4 +8,21 @@ class Game < ApplicationRecord
   validates :number_of_players, numericality: { greater_than_or_equal_to: 2,
                                                 less_than_or_equal_to: 6 },
                                 presence: true
+
+  after_create :create_hexagons
+
+  private
+
+  def create_hexagons
+    min = board_size * -1
+    max = board_size
+
+    (min..max).each do |x|
+      (min..max).each do |y|
+        hexagons.create(x: x, y: y)
+      end
+    end
+
+    hexagons.reset
+  end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Create automatically the hexagons' do
+    game = create(:game, board_size: 3, hexagons: [])
+
+    expected_coordinates = [
+      [-3, 0, 3], [-3, 1, 2], [-3, 2, 1], [-3, 3, 0], [-2, -1, 3],
+      [-2, 0, 2], [-2, 1, 1], [-2, 2, 0], [-2, 3, -1], [-1, -2, 3],
+      [-1, -1, 2], [-1, 0, 1], [-1, 1, 0], [-1, 2, -1], [-1, 3, -2],
+      [0, -3, 3], [0, -2, 2], [0, -1, 1], [0, 0, 0], [0, 1, -1],
+      [0, 2, -2], [0, 3, -3], [1, -3, 2], [1, -2, 1], [1, -1, 0],
+      [1, 0, -1], [1, 1, -2], [1, 2, -3], [2, -3, 1], [2, -2, 0],
+      [2, -1, -1], [2, 0, -2], [2, 1, -3], [3, -3, 0], [3, -2, -1],
+      [3, -1, -2], [3, 0, -3]
+    ]
+
+    assert_equal 37, game.hexagons.size
+    assert_equal expected_coordinates, game.hexagons.map { |h| [h.x, h.y, h.z] }
+  end
 end
